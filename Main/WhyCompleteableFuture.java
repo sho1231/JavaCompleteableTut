12077dd15d765e2a7a4e4c025c29bdce64091602
Shourja Ganguly <shourja.ganguly181@gmail.com>
2024-08-11 19:58:55 +0530

    1st part done

--- Main/WhyCompleteableFuture.java
+++ Main/WhyCompleteableFuture.java
@@ -1,8 +1,46 @@
 package Main;
 
-public class WhyCompleteableFuture {
+import java.util.concurrent.*;
+import java.util.*;
 
-  public static void main(String[] args) {
+public class WhyCompleteableFuture {
+  public static void delay(int s) {
+    try {
+      TimeUnit.SECONDS.sleep(s);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+  public static List<Integer> supplyAsync() throws ExecutionException, InterruptedException {
+    CompletableFuture<List<Integer>> f = CompletableFuture.supplyAsync(()-> Arrays.asList(1,2,3,4));
+    return f.get();
+  }
+  public static List<Integer> supplyAsyncWithExecutor(ExecutorService executorService) throws ExecutionException, InterruptedException {
+    delay(6);
+    CompletableFuture<List<Integer>> f = CompletableFuture.supplyAsync(()-> Arrays.asList(1,2,3,4),executorService);
+    return f.get();
+  }
+  public static Void runasyncMethod() throws ExecutionException, InterruptedException {
+    CompletableFuture<Void> f = CompletableFuture.runAsync(()->{
+      delay(6);
+      System.out.println(1);
+    });
+   return f.get();
+  }
+  public static Void runasyncMethodWithCustomExecutor() throws ExecutionException, InterruptedException {
+    CompletableFuture<Void> f = CompletableFuture.runAsync(()->{
+      delay(6);
+      System.out.println(1);
+    }, Executors.newFixedThreadPool(4));
+    return f.get();
+  }
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
     // complete method of the completeablefuture can stop the task forcefully
+//    System.out.println(runasyncMethodWithCustomExecutor());
+//    System.out.println(12323);
+      ExecutorService executorService = Executors.newFixedThreadPool(4);
+    System.out.println(supplyAsyncWithExecutor(executorService));;
+    executorService.shutdown();
   }
 }
--- out/production/JavaCompleteableTut/.idea/sonarlint/securityhotspotstore/index.pb
+++ out/production/JavaCompleteableTut/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,7 +1,7 @@
 
 9
 	notes.txt,8/3/83348d283c1ba0b3b4d55fdc04c39a165b933125
-C
-Main/Asynchron.java,1/f/1f5787037dec43835346d3be29a031f83495daa5
 F
 Main/WhyNotFuture.java,e/8/e86f949ca3ff277379a8493bfbfb2442f7ffb742
+O
+Main/WhyCompleteableFuture.java,5/5/55e3809fb45e167776602a0b403156349e591d13
