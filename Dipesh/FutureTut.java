2bd0bf5c50d270029058f2cfae0af4edb7cd4a95
sho1231 <38104743+sho1231@users.noreply.github.com>
2024-11-02 21:52:24 +0530

    dipesh yt channel tut

---
+++ Dipesh/FutureTut.java
@@ -0,0 +1,48 @@
+package Dipesh;
+
+import java.util.concurrent.*;
+
+public class FutureTut {
+    public static void delay(int delay) {
+        try{
+            TimeUnit.SECONDS.sleep(delay);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void asyncUsingFuture() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> f = executorService.submit(()->{
+            delay(4);
+            System.out.println("Test 123");
+            return "123";
+        });
+//        System.out.println("f:"+f.get());
+        executorService.shutdown();
+    }
+    public static CompletableFuture<Void> usingRunAsync() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        CompletableFuture<Void> c = CompletableFuture.runAsync(()->{
+            delay(3);
+            System.out.println("Thread name:"+Thread.currentThread().getName());
+        },executorService);
+        executorService.shutdown();
+        return c;
+    }
+
+    public static CompletableFuture<String> usingSupplyAsync() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        CompletableFuture<String> c = CompletableFuture.supplyAsync(()->{
+            delay(3);
+            System.out.println("Thread name:"+Thread.currentThread().getName());
+            return "OK";
+        },executorService);
+        executorService.shutdown();
+        return c;
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        usingSupplyAsync().join();
+        System.out.println("Done");
+    }
+}
--- out/production/JavaCompleteableTut/.idea/sonarlint/securityhotspotstore/index.pb
+++ out/production/JavaCompleteableTut/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,16 +1,6 @@
 
 9
 	notes.txt,8/3/83348d283c1ba0b3b4d55fdc04c39a165b933125
-F
-Main/WhyNotFuture.java,e/8/e86f949ca3ff277379a8493bfbfb2442f7ffb742
-O
-Main/WhyCompleteableFuture.java,5/5/55e3809fb45e167776602a0b403156349e591d13
->
-Main/Test.java,1/d/1dcbdab6d09d23199c2b32b61f614c2b3abe28f7
-M
-Main/CompleteablePartTwo.java,8/e/8ee95c1a1b98460cba8c49ffdf93f3f1016c8769
-H
-Main/ApplyAndAccept.java,1/a/1a37a39a7b9d413f70916277b0b6fa272c2acde3
 K
 Main/Completeable/Test.java,7/8/78002cbccef0eadb802b4e0289156b5734c48bb2
 U
